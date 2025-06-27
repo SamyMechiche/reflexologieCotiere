9a64c8b5b4e1f2ee86fa25466d3d7307afee607f
SamyMechiche <mechiche.samysm@gmail.com>
2025-06-27 16:28:44 +0200

    💄Reworked homepage design because of missing meta info in base html twig💄

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompanyController extends AbstractController
+{
+    #[Route('/company', name: 'company')]
+    public function index(): Response
+    {
+        return $this->render('company.html.twig');
+    }
+} 
